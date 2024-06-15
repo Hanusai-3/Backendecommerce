38f26de0be9d1f314d232d704732550bc348ec4d
hanu <saihanu309@gmail.com>
2024-06-15 12:28:38 +0530

    second commit

--- ecommerceapp/src/main/java/com/ecommerceapp/ecommerceapp/model/CartItem.java
+++ ecommerceapp/src/main/java/com/ecommerceapp/ecommerceapp/model/CartItem.java
@@ -1,9 +1,10 @@
 package com.ecommerceapp.ecommerceapp.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-
+@Data
 public class CartItem {
 
     @Id
@@ -11,55 +12,11 @@ public class CartItem {
     private String productId;
     private String title;
     private String description;
+    private String imgSrc;
     private double price;
     private int quantity;
 
     // Getters and setters
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 }
